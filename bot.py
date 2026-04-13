9f0e03cad9838a21eed6748fcd6beb26532097f6
aakurov <rldbta@gmail.com>
2026-04-13 12:33:29 +0300

    Enhance bot functionality for event notifications
    Refactor Telegram message sending and process ICS events.

--- bot.py
+++ bot.py
@@ -1,16 +1,45 @@
 import os
 import requests
+from datetime import datetime, timezone
 
 TOKEN = os.environ["TG_TOKEN"]
 CHAT_ID = os.environ["TG_CHAT"]
+ICS_URL = os.environ["ICS_URL"]
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+def send(text):
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={"chat_id": CHAT_ID, "text": text}
+    )
 
-data = {
-    "chat_id": CHAT_ID,
-    "text": "🚀 GitHub workflow запустился"
-}
+r = requests.get(ICS_URL)
 
-r = requests.post(url, data=data)
+lines = r.text.splitlines()
 
-print(r.text)
+events = []
+
+summary = None
+dtstart = None
+
+for line in lines:
+
+    if line.startswith("SUMMARY:"):
+        summary = line.replace("SUMMARY:", "").strip()
+
+    if "DTSTART" in line:
+        dtstart = line.split(":")[1].strip()
+
+    if line.startswith("END:VEVENT"):
+        if summary and dtstart:
+            events.append((summary, dtstart))
+
+        summary = None
+        dtstart = None
+
+
+msg = "📅 Найденные события:\n\n"
+
+for name, t in events[:10]:
+    msg += f"{name} | {t}\n"
+
+send(msg)
